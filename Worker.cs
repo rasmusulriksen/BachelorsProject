ad256927d23af53ab45657f25cc5fbe23efe41dc
Rasmus Ulriksen <rasmus.ulriksen@visma.com>
2025-01-28 08:15:11 +0100

    Nothing

--- Worker.cs
+++ Worker.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Dapr.Client;
 
 namespace EmailService;
 
